174bd50cfa300e70308a5c7be6f6873f3b3869e8
Ricardo <chaneque500@gmail.com>
2014-06-01 16:49:45 -0500

    Info user

--- db/migrate/20140601214524_add_info_to_user.rb
+++ db/migrate/20140601214524_add_info_to_user.rb
@@ -1,4 +1,6 @@
 class AddInfoToUser < ActiveRecord::Migration
   def change
+  	add_column :users, :name, :string, :default => nil
+  	add_column :users, :empleyee_number, :integer, :default => nil
   end
 end
