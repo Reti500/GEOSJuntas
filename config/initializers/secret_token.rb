ee3309b554fb624dc9f0b93f0fb8ddd8293da41a
Ricardo <chaneque500@gmail.com>
2014-06-01 16:46:04 -0500

    Iniciando el proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GEOSJuntas::Application.config.secret_key_base = '2bc4bbef3b4749a713f4c7c499f302e68ed7811ef2ea93a3b8b92f0fab2cfac9b7aeac41598c0adb2922f34b9d299dbbccc3aa6742a1d43b0224f10a922227f0'
